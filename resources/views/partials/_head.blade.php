97970f14c34f38c3ade8af913106f6d6943b3719
Ikoooo <ikoooo@Ikoooos-MacBook-Air.local>
2024-06-05 16:37:35 +0700

    pertemuan8

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -6,6 +6,6 @@
     <meta name="author" content="" />
     <title>Dashboard - MANTU | Manajemen Tugas</title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
-    <link href="{{ asset('sbadmin')}}/css/styles.css" rel="stylesheet" />
+    <link href="{{ asset('sbadmin') }}/css/styles.css" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 </head>
--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,16 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('tamplate');
+    return view('dashboard.index');
 });
+
 /**
- * HTTP Method:
+ * HTTP Methods:
  * 1. Get: untuk menampilkan sesuatu
- * 2. Post: Untuk menambahkan data
+ * 2. Post: untuk menambahkan data
  * 3. Put: untuk mengubah data
- * 4. Delete; untuk menghapus data
+ * 4. Delete: untuk menghapus data
  */
 
-// Rute untuk menampilkan halaman daftar tugas
-Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-
-// Rute untuk menampilkan halaman detail tugas
-Route::get('/tasks/{id}', [TaskController::class, 'detail'])->name('task.detail');
+// Route untuk menampilkan halaman daftar tugas
+Route::get('/tasks', [TaskController::class, 'index']);
